4d5027a8bb2649a729f38fd9b25503ea592c79a1
Szymon Kedra <s5zee@wp.pl>
2022-11-24 18:11:31 +0100

    added Skillset component with image

---
+++ src/Components/Container/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border-radius: 4px;
+  background: #ffffff;
+  max-width: 1216px;
+  margin: auto;
+  padding: 32px;
+  margin-bottom: 72px;
+`;
