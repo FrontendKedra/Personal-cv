6e4af2877b1cf7747ce9a373a7939b0d5866405d
Szymon Kedra <s5zee@wp.pl>
2022-11-27 00:02:57 +0100

    removed unused action creator,changed initialState

--- src/Components/features/reposSlice.js
+++ src/Components/features/reposSlice.js
@@ -4,15 +4,12 @@ const reposSlice = createSlice({
   name: "repos",
   initialState: {
     repos: [],
-    reposState: { loading: false, success: false, error: false },
+    reposState: { loading: true, success: false, error: false },
   },
   reducers: {
     fetchReposResponse: () => {},
     setRepos: (state, { payload }) => {
       state.repos = payload;
-    },
-    reposLoading: ({ reposState }) => {
-      reposState.loading = true;
     },
     reposSuccess: ({ reposState }) => {
       reposState.loading = false;
@@ -29,11 +26,11 @@ const reposSlice = createSlice({
 export const {
   fetchReposResponse,
   setRepos,
-  reposLoading,
   reposSuccess,
   reposError,
 } = reposSlice.actions;
 
 export const selectReposState = (state) => state.repos;
 export const selectRepos = (state) => selectReposState(state).repos;
+export const selectReposCurrentState = (state) => selectReposState(state).reposState;
 export default reposSlice.reducer;
