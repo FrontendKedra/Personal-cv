de9157935646173f09f32400f82b6d5b3c54c48d
Szymon Kedra <s5zee@wp.pl>
2022-11-27 00:05:16 +0100

    api from github, different reposStates management

--- src/Components/features/Porfolio/index.js
+++ src/Components/features/Porfolio/index.js
@@ -20,128 +20,72 @@ import {
 import { ReactComponent as IconSpinner } from "../../../images/icon-spinner.svg";
 import { ReactComponent as DangerIcon } from "../../../images/Danger.svg";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchReposResponse, selectRepos } from "../reposSlice";
+import {
+  fetchReposResponse,
+  selectRepos,
+  selectReposCurrentState,
+} from "../reposSlice";
 import { useEffect } from "react";
 
 export const Portfolio = () => {
   const dispatch = useDispatch();
   const repos = useSelector(selectRepos);
+  const reposState = useSelector(selectReposCurrentState);
 
   useEffect(() => {
     dispatch(fetchReposResponse());
   }, [dispatch]);
-
-  console.log(repos);
 
   return (
     <PortfolioContainer>
       <GithubIcon />
       <PortfolioParagraph>Portfolio</PortfolioParagraph>
       <Span>My recent projects</Span>
-      <GithubReposContainer>
-        <RepositoryContainer>
-          <RepositoryHeader>Movies Browser</RepositoryHeader>
-          <Description>
-            Project description, e.g. website where you can search for favourite
-            movies and people. Project description, e.g. website where you can
-            search.
-          </Description>
-          <LinkContainer>
-            <Description>Demo:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-          <LinkContainer>
-            <Description>Code:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-        </RepositoryContainer>
-        <RepositoryContainer>
-          {" "}
-          <RepositoryHeader>Movies Browser</RepositoryHeader>
-          <Description>
-            Project description, e.g. website where you can search for favourite
-            movies and people. Project description, e.g. website where you can
-            search.
-          </Description>
-          <LinkContainer>
-            <Description>Demo:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-          <LinkContainer>
-            <Description>Code:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-        </RepositoryContainer>
-        <RepositoryContainer>
-          {" "}
-          <RepositoryHeader>Movies Browser</RepositoryHeader>
-          <Description>
-            Project description, e.g. website where you can search for favourite
-            movies and people. Project description, e.g. website where you can
-            search.
-          </Description>
-          <LinkContainer>
-            <Description>Demo:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-          <LinkContainer>
-            <Description>Code:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-        </RepositoryContainer>
-        <RepositoryContainer>
-          {" "}
-          <RepositoryHeader>Movies Browser</RepositoryHeader>
-          <Description>
-            Project description, e.g. website where you can search for favourite
-            movies and people. Project description, e.g. website where you can
-            search.
-          </Description>
-          <LinkContainer>
-            <Description>Demo:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-          <LinkContainer>
-            <Description>Code:</Description>
-            <RepositoryLink href="https://link.demo.com">
-              https://link.demo.com
-            </RepositoryLink>
-          </LinkContainer>
-        </RepositoryContainer>
-      </GithubReposContainer>
-      <LoadingParagraph>
-        Please wait, projects are being loaded...
-      </LoadingParagraph>
-      <Icon>
-        <IconSpinner />
-      </Icon>
-      <ErrorContainer>
-        <IconParagraph>
-          <DangerIcon />
-        </IconParagraph>
-        <DangerParagraph>Ooops! Something went wrong...</DangerParagraph>
-        <ErrorText>
-          Sorry, failed to load Github projects.
-          <br /> You can check them directly on Github.
-        </ErrorText>
-        <GithubButton as="a" href="https://github.com/s5zee">
-          Go To Github
-        </GithubButton>
-      </ErrorContainer>
+      {reposState.loading === true ? (
+        <>
+          <LoadingParagraph>
+            Please wait, projects are being loaded...
+          </LoadingParagraph>
+          <Icon>
+            <IconSpinner />
+          </Icon>
+        </>
+      ) : reposState.error === true ? (
+        <ErrorContainer>
+          <IconParagraph>
+            <DangerIcon />
+          </IconParagraph>
+          <DangerParagraph>Ooops! Something went wrong...</DangerParagraph>
+          <ErrorText>
+            Sorry, failed to load Github projects.
+            <br /> You can check them directly on Github.
+          </ErrorText>
+          <GithubButton as="a" href="https://github.com/s5zee">
+            Go To Github
+          </GithubButton>
+        </ErrorContainer>
+      ) : (
+        <GithubReposContainer>
+          {repos.map((repo) => (
+            <RepositoryContainer key={repo.id}>
+              <RepositoryHeader>{repo.name}</RepositoryHeader>
+              <Description>{repo.description}</Description>
+              <LinkContainer>
+                <Description>Demo:</Description>
+                <RepositoryLink href={repo.homepage}>
+                  {repo.homepage}
+                </RepositoryLink>
+              </LinkContainer>
+              <LinkContainer>
+                <Description>Code:</Description>
+                <RepositoryLink href={repo.html_url}>
+                  {repo.html_url}
+                </RepositoryLink>
+              </LinkContainer>
+            </RepositoryContainer>
+          ))}
+        </GithubReposContainer>
+      )}
     </PortfolioContainer>
   );
 };
