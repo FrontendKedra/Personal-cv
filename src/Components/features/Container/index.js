1e06efaa068396d322c03a60cd7f43d29a7977b9
Szymon Kedra <s5zee@wp.pl>
2023-01-02 17:07:09 +0100

    moved styled component to common\Tile

--- src/Components/features/Container/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  border-radius: 4px;
-  background: #ffffff;
-  max-width: 1216px;
-  padding: 32px;
-  margin-bottom: 72px;
-`;
