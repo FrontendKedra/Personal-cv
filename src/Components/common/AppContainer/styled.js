4981089e2ec437a7ed7e6851a4addec65ad96fc8
Szymon Kedra <s5zee@wp.pl>
2023-01-02 17:04:48 +0100

    AppContainer moved to common

---
+++ src/Components/common/AppContainer/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  max-width: 1216px;
+  margin: auto;
+  padding: 110px 16px;
+
+  @media (max-width: ${({theme}) => theme.breakpoints.large}) {
+  padding: 50px 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.medium}) {
+  padding: 36px 16px;
+  }
+
+  @media (max-width: ${({theme}) => theme.breakpoints.small}) {
+  padding: 0 16px;
+  }
+`;
--- src/Components/features/AppContainer/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const AppContainer = styled.div`
-  max-width: 1216px;
-  margin: auto;
-  padding: 110px 0;
-`;
