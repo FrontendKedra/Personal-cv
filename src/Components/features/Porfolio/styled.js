4ddf421b8d48edfa2f589640501c0463937dbe21
Szymon Kedra <s5zee@wp.pl>
2023-01-02 17:25:59 +0100

    mediaqueries added, colors and breakpoints from theme,

--- src/Components/features/Porfolio/styled.js
+++ src/Components/features/Porfolio/styled.js
@@ -11,13 +11,24 @@ export const PortfolioParagraph = styled.p`
   font-size: 30px;
   margin: 0;
   padding-top: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 18px;
+    line-height: 22px;
+  }
 `;
 
 export const Span = styled.span`
   margin-top: 8px;
   font-weight: 400;
   font-size: 20px;
-  color: #252525;
+  color: ${({ theme }) => theme.colors.mineShaft};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 17px;
+    line-height: 140%;
+    margin-top: 16px;
+  }
 `;
 
 export const GithubReposContainer = styled.div`
@@ -26,6 +37,14 @@ export const GithubReposContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 32px;
   margin-top: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    gap: 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const RepositoryContainer = styled.div`
@@ -35,26 +54,42 @@ export const RepositoryContainer = styled.div`
   justify-content: start;
   gap: 24px;
   border-radius: 4px;
-  background-color: #ffffff;
-  border: 6px solid #d1d5da4d;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 6px solid ${({ theme }) => theme.colors.iron};
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
     0px 16px 58px rgba(9, 10, 51, 0.03);
 
   &:hover {
-    border: 6px solid #0366d633;
+    transition: 0.3s;
+    border: 6px solid ${({ theme }) => theme.colors.anakiwa};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    gap: 16px;
+    padding: 18px;
   }
 `;
 
 export const RepositoryHeader = styled.header`
   font-weight: 700;
   font-size: 24px;
-  color: #0366d6;
+  color: ${({ theme }) => theme.colors.scienceBlue};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 16px;
+    line-height: 19px;
+  }
 `;
 
 export const Description = styled.article`
   font-weight: 400;
   font-size: 18px;
-  color: #6e7e91;
+  color: ${({ theme }) => theme.colors.slateGray};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 14px;
+    line-height: 17px;
+  }
 `;
 
 export const LinkContainer = styled.div`
@@ -65,35 +100,42 @@ export const LinkContainer = styled.div`
 `;
 
 export const RepositoryLink = styled.a`
-  color: #0366d6;
+  color: ${({ theme }) => theme.colors.scienceBlue};
   border-bottom: 1px solid #9999ff;
   text-decoration: none;
 
   &:hover {
-    border-bottom: 1px solid #0366d6cc;
+    transition: 0.3s;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.scienceBlue};
+    color: ${({ theme }) => theme.colors.blue};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 14px;
+    line-height: 17px;
   }
 `;
 
 export const LoadingParagraph = styled.p`
   font-weight: 400;
   font-size: 20px;
-  color: #252525;
+  color: ${({ theme }) => theme.colors.mineShaft};
   margin: 88px 0 48px 0;
 `;
 
 export const DangerParagraph = styled.p`
   font-weight: 700;
   font-size: 24px;
-  color: #252525;
+  color: ${({ theme }) => theme.colors.mineShaft};
   margin: 24px 0 32px 0;
 `;
 
 export const GithubButton = styled.button`
-  background-color: #0366d6;
+  background-color: ${({ theme }) => theme.colors.scienceBlue};
   text-decoration: none;
   font-weight: 600;
   font-size: 20.0584px;
-  color: #ffffff;
+  color: ${({ theme }) => theme.colors.white};
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -104,22 +146,23 @@ export const GithubButton = styled.button`
   max-width: 168px;
 
   &:hover {
-    box-shadow: 2px -2px 0px #8cc2ff, -2px 2px 0px #8cc2ff;
+    transition: 0.3s;
+    box-shadow: 2px -2px 0px ${({ theme }) => theme.colors.anakiwa},
+      -2px 2px 0px ${({ theme }) => theme.colors.anakiwa};
   }
 `;
 
 export const Icon = styled.p`
   animation: rotation 1s infinite linear;
 
-
-@keyframes rotation {
-  from {
-    transform: rotate(0deg);
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
   }
-  to {
-    transform: rotate(360deg);
-  }
-}
 `;
 
 export const ErrorContainer = styled.div`
@@ -131,7 +174,7 @@ export const ErrorContainer = styled.div`
 export const ErrorText = styled.article`
   font-weight: 400;
   font-size: 20px;
-  color: #252525;
+  color: ${({ theme }) => theme.colors.mineShaft};
   margin-bottom: 32px;
   text-align: center;
 `;
