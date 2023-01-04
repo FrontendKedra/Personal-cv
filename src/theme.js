50572b818fe99513b544587e774f7292631096e7
Szymon Kedra <s5zee@wp.pl>
2023-01-04 17:45:58 +0100

    theme added with lightTheme and darkTheme

---
+++ src/theme.js
@@ -0,0 +1,57 @@
+export const lightTheme = {
+  colors: {
+    iron: "#d1d5da",
+    gray: "#d1d5da4d",
+    slateGray: "#6e7e91",
+    mineShaft: "#252525",
+    white: "#ffffff",
+    scienceBlue: "#0366d6",
+    lightBlue: "#0366d633",
+    melrose: "#9999FF",
+    anakiwa: "#8cc2ff",
+    mercury: "#e5e5e5",
+  },
+  breakpoints: {
+    small: "480px",
+    medium: "767px",
+    large: "921px",
+  },
+  borderRadius: {
+    small: "4px",
+    medium: "30px",
+    large: "50px",
+  },
+  shadows: {
+    hoverShadow:
+      "2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    iron: "#E5E5E5",
+    gray: "#d1d5da1a",
+    slateGray: "#ffffff",
+    mineShaft: "#ffffff",
+    white: "#363636b8",
+    scienceBlue: "#2188FF",
+    lightBlue: "#0366d680",
+    melrose: "#2196f342",
+    mercury: "#252525",
+  },
+  breakpoints: {
+    small: "480px",
+    medium: "767px",
+    large: "921px",
+  },
+  borderRadius: {
+    small: "4px",
+    medium: "30px",
+    large: "50px",
+  },
+  shadows: {
+    repositoryShadow: "0px -2px 50px #090a3305, 0px 16px 58px #090a3308",
+    hoverShadow:
+      "2px -2px 0px #6D93BE, -2px 2px 0px #6D93BE, 2px 2px 0px #6D93BE, -2px -2px 0px #6D93BE",
+  },
+};
