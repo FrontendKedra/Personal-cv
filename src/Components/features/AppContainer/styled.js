555c0bfd0a754570d7f879d2e47fbd4229c11b03
Szymon Kedra <s5zee@wp.pl>
2023-01-10 19:35:08 +0100

    formatting fixed

--- src/Components/features/AppContainer/styled.js
+++ src/Components/features/AppContainer/styled.js
@@ -5,15 +5,15 @@ export const AppContainer = styled.div`
   margin: auto;
   padding: 110px 16px;
 
-  @media (max-width: ${({theme}) => theme.breakpoints.large}) {
-  padding: 50px 16px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+    padding: 50px 16px;
   }
 
-  @media (max-width: ${({theme}) => theme.breakpoints.medium}) {
-  padding: 36px 16px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    padding: 36px 16px;
   }
 
-  @media (max-width: ${({theme}) => theme.breakpoints.small}) {
-  padding: 0 16px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    padding: 0 16px;
   }
 `;
