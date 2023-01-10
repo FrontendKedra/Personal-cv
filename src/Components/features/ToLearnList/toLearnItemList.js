2e12ff5057f5c5cb88c78bac48d2c3042e258703
Szymon Kedra <s5zee@wp.pl>
2023-01-10 12:16:58 +0100

    SkillsetList

--- src/Components/features/ToLearnList/toLearnItemList.js
+++ src/Components/features/ToLearnList/toLearnItemList.js
@@ -1,6 +1,5 @@
 export const toLearnItemList = [
-  { content: "React Query", id: 1 },
-  { content: "TypeScript", id: 2 },
-  { content: "JS classes", id: 3 },
-  { content: "React-router 6.0", id: 4 },
+  { content: "TypeScript", id: 1 },
+  { content: "React Query", id: 2 },
+  { content: "React-router 6.0", id: 3 },
 ];
