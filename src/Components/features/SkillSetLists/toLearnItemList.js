0cfd52e3a50c06538059a969a2c1c975b372339e
Szymon Kedra <s5zee@wp.pl>
2023-01-31 19:36:53 +0100

    toLearnItemList updated

--- src/Components/features/SkillSetLists/toLearnItemList.js
+++ src/Components/features/SkillSetLists/toLearnItemList.js
@@ -1,5 +1,5 @@
 export const toLearnItemList = [
-  { content: "TypeScript", id: 1 },
+  { content: "TypeScript(practice)", id: 1 },
   { content: "React Query", id: 2 },
   { content: "React-router 6.0", id: 3 },
 ];
