64484bd550cd43e67ccfddd737ed7eb5f260ccf6
Szymon Kedra <s5zee@wp.pl>
2023-02-07 15:18:06 +0100

    backgroundSlice adjusted to typescript InitialState, initistalState and RootState added

--- src/Components/features/Introduction/backgroundSlice.ts
+++ src/Components/features/Introduction/backgroundSlice.ts
@@ -1,11 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../../store";
 import { getBackgroundFromLocalStorage } from "./backgroundLocalStorage";
+
+interface InitialState {
+  darkBackground: boolean;
+}
+
+const initialState: InitialState = {
+  darkBackground: getBackgroundFromLocalStorage(),
+};
 
 const backgroundSlice = createSlice({
   name: "background",
-  initialState: {
-    darkBackground: getBackgroundFromLocalStorage(),
-  },
+  initialState,
   reducers: {
     toggleBackgroundColor: (state) => {
       state.darkBackground = !state.darkBackground;
@@ -15,8 +22,8 @@ const backgroundSlice = createSlice({
 
 export const { toggleBackgroundColor } = backgroundSlice.actions;
 
-export const selectBackgroundState = (state) => state.background;
-export const selectDarkBackground = (state) =>
+export const selectBackgroundState = (state: RootState) => state.background;
+export const selectDarkBackground = (state: RootState) =>
   selectBackgroundState(state).darkBackground;
 
 export default backgroundSlice.reducer;
