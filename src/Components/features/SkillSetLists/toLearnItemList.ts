a60629ad560be60da6c57d96156eb01a9c2cf0b9
Szymon Kedra <s5zee@wp.pl>
2023-02-17 11:12:09 +0100

    updated toLearnList

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -1,7 +1,7 @@
 import { Lists } from "../../../@types/listsTypes";
 
 export const toLearnItemList: Lists[] = [
-  { content: "TypeScript(practice)", id: 1 },
-  { content: "React Query", id: 2 },
+  { content: "React Query", id: 1 },
+  { content: "React Testing Library", id: 2 },
   { content: "React-router 6.0", id: 3 },
 ];
