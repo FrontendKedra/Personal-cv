0b1bf3db13c456f54007008eb97102189b3c3ff7
Szymon Kedra <s5zee@wp.pl>
2023-03-19 23:05:08 +0100

    tolearn list updated

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -1,6 +1,7 @@
 import { Lists } from "../../../@types/typesAndInterfaces";
 
 export const toLearnItemList: Lists[] = [
-  { content: "React Testing Library", id: 1 },
-  { content: "React-router 6.0", id: 2 },
+  { content: "Jest", id: 1 },
+  { content: "React Testing Library", id: 2 },
+  { content: "React-router 6.0", id: 3 },
 ];
