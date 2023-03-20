32f7ca387af244e56ce6048e805d072a13df3cb4
Szymon Kedra <s5zee@wp.pl>
2023-03-20 23:01:42 +0100

    new technology added toLearnList

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -4,4 +4,5 @@ export const toLearnItemList: Lists[] = [
   { content: "Jest", id: 1 },
   { content: "React Testing Library", id: 2 },
   { content: "React-router 6.0", id: 3 },
+  { content: "TanStack Query", id: 4 },
 ];
