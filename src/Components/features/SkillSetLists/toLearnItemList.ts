590ce1829e67a0bde7898a602bf237b9f4eb1ecb
Szymon Kedra <s5zee@wp.pl>
2023-05-17 14:05:32 +0200

    added new to learn item

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -3,4 +3,5 @@ import { Lists } from "../../../@types/typesAndInterfaces";
 export const toLearnItemList: Lists[] = [
   { content: "TanStack Query", id: 1 },
   { content: "Jira", id: 2 },
+  { content: "Docker", id: 3 },
 ];
