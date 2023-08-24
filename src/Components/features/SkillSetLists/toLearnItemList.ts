1585afa239def74837123fe21fa6c208e2e4ed29
Szymon Kedra <s5zee@wp.pl>
2023-08-24 19:26:34 +0200

    tolearnlist updated

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -3,5 +3,5 @@ import { Lists } from "../../../@types/typesAndInterfaces";
 export const toLearnItemList: Lists[] = [
   { content: "TanStack Query", id: 1 },
   { content: "Next.js", id: 2 },
-  { content: "Jira", id: 3 },
+  { content: "Master all skills above", id: 3 },
 ];
