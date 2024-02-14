7e2448072ac0d081740a22fea173c51dcc580b0d
Szymon Kedra <s5zee@wp.pl>
2024-02-14 21:12:40 +0100

    adjustment to toLearnItemList

--- src/Components/features/SkillSetLists/toLearnItemList.ts
+++ src/Components/features/SkillSetLists/toLearnItemList.ts
@@ -3,5 +3,5 @@ import { Lists } from "../../../@types/typesAndInterfaces";
 export const toLearnItemList: Lists[] = [
   { content: "Docker", id: 1 },
   { content: "Master Next.js app router", id: 2 },
-  { content: "Improve my GraphQL knowledge", id: 3 },
+  { content: "Improve GraphQL knowledge", id: 3 },
 ];
